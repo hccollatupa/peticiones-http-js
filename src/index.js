18ee1d31f85b498cde19d784e53597d7e10c3a99
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-28 16:06:38 -0500

    Protip - Promesas en cadena

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const jokeURL = 'https://api.chucknorris.io/jokes/random';
+/*
 fetch(jokeURL).then(resp => {
     //console.log({ resp });
     resp.json().then(({ id, value }) => {
@@ -6,3 +7,10 @@ fetch(jokeURL).then(resp => {
         console.log(value);
     });
 });
+*/
+
+fetch(jokeURL)
+    .then(resp => resp.json())
+    .then(({ id, value }) => {
+        console.log(id, value);
+    });
