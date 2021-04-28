dab03aa0117788e7496696221095861e6b9f39f3
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-28 18:42:37 -0500

    CRUD - Leer usuarios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import { init } from './js/usuarios-page';
+//import { init } from './js/chistes-page';
+//import { init } from './js/usuarios-page';
+import * as CRUD from './js/crud-provider';
 
-init();
+CRUD.getUsuario(2).then(console.log);
